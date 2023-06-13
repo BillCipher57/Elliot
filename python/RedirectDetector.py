0efaa18a1839d867f134de12fcd8eccff488b622
Mechanic <74297154+TheMechanic57@users.noreply.github.com>
2023-06-13 20:41:32 +0100

    removed virtual environment and electron

--- python/RedirectDetector.py
+++
@@ -1,7 +0,0 @@
-import requests
-import sys
-sys.dont_write_bytecode = True
-r = requests.get(sys.argv[1])
-
-for x in r.history:
-    print(x.url, end="\n\n", flush=True)
--- python/Whois.py
+++
@@ -1,5 +0,0 @@
-import whois
-import sys
-sys.dont_write_bytecode = True
-res = whois.whois(sys.argv[1])
-print(res, flush=True)
